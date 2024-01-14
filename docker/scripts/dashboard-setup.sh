1ee771db291152e4208960533c6902e993e6d842
Viktor Vsk <me@viktorvsk.com>
2024-01-14 11:38:35 +0100

    Default docker-compose.yaml sets up default user for Pulsar Manager on start

---
+++ docker/scripts/dashboard-setup.sh
@@ -0,0 +1,2 @@
+CSRF_TOKEN=$(curl -v "http://dashboard:7750/pulsar-manager/csrf-token")
+curl -H "X-XSRF-TOKEN: $CSRF_TOKEN" -H "Cookie: XSRF-TOKEN=$CSRF_TOKEN;" -H 'Content-Type: application/json' -X PUT "http://dashboard:7750/pulsar-manager/users/superuser" -d '{"name": "knowstr", "password": "knowstr", "description": "test", "email": "username@test.org"}'
