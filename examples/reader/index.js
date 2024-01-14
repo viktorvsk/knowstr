e51e88f0bdbd53bb342d8741ba6fdeea14691318
Viktor Vsk <me@viktorvsk.com>
2024-01-14 11:40:08 +0100

    Add example reader implementation to use Pulsar; in default docker-compose.yaml it syncs Redis seen events with what actually present in Pulsar

---
+++ examples/reader/index.js
@@ -0,0 +1,62 @@
+const Pulsar = require("pulsar-client");
+const Redis = require("redis");
+
+const READ_TIMEOUT = parseInt(process.env.READ_TIMEOUT || 5000);
+
+(async () => {
+  let exiting;
+  let msg;
+  let timeout;
+
+  const redisClient = Redis.createClient({
+    url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+  }).on("error", (err) => console.error("[RedisClientError]", err));
+
+  await redisClient.connect();
+
+  const [topic, token, url] = await Promise.all([redisClient.HGET("settings", "pulsar_topic"), redisClient.HGET("settings", "pulsar_token"), redisClient.HGET("settings", "pulsar_url")]);
+
+  const currentMessageId = await redisClient.GET(`pulsar_reader_example_message_id:${topic}`);
+
+  const clientParams = {
+    serviceUrl: url || process.env.PULSAR_URL || "pulsar://127.0.0.1:6650",
+    operationTimeoutSeconds: 30,
+  };
+
+  if (token || process.env.PULSAR_TOKEN) {
+    clientParams["authentication"] = new Pulsar.AuthenticationToken({
+      token: token || process.env.PULSAR_TOKEN,
+    });
+  }
+
+  // Create a client
+  const client = new Pulsar.Client(clientParams);
+
+  const mid = currentMessageId ? Pulsar.MessageId.deserialize(Buffer.from(currentMessageId, "hex")) : Pulsar.MessageId.earliest();
+
+  // Create a reader
+  const reader = await client.createReader({
+    topic: topic,
+    startMessageId: mid,
+  });
+
+  const onTimeout = async () => {
+    console.log("Exiting because of reader timeout");
+    await reader.close().catch(console.error);
+    await client.close().catch(console.error);
+    await redisClient.quit().catch(console.error);
+    process.exit();
+  };
+
+  timeout = setTimeout(onTimeout, READ_TIMEOUT);
+
+  while ((msg = await reader.readNext())) {
+    clearTimeout(timeout);
+    timeout = setTimeout(onTimeout, READ_TIMEOUT);
+    const payload = msg.getData().toString();
+    const event = JSON.parse(payload);
+    const messageId = msg.getMessageId().serialize().toString("hex");
+
+    await Promise.all([redisClient.SET(`pulsar_reader_example_message_id:${topic}`, messageId), redisClient.SET(`id:${event.id}`, parseInt(Date.now() / 1000))]);
+  }
+})();
