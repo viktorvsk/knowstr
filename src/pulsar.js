cdf5ca045beebdf82e346d9866e5c0648e827284
Viktor Vsk <me@viktorvsk.com>
2024-01-14 11:35:04 +0100

    Pulsar producer to conditionally authenticate with JWT

--- src/pulsar.js
+++ src/pulsar.js
@@ -1,16 +1,33 @@
 import PulsarClient from "pulsar-client";
-import { pulsar_topic, pulsar_send_timeout_ms, pulsar_max_pending_messages, pulsar_block_if_queue_full } from "./settings.js";
+import { pulsar_topic, pulsar_send_timeout_ms, pulsar_max_pending_messages, pulsar_block_if_queue_full, pulsar_token, pulsar_url } from "./settings.js";
 
-const URL = process.env.PULSAR_URL || "pulsar://127.0.0.1:6650";
+const clientParams = {
+  serviceUrl: pulsar_url || process.env.PULSAR_URL || "pulsar://127.0.0.1:6650",
+  // operationTimeoutSeconds: 30,
+  // ioThreads: 4,
+  // messageListenerThreads: 4,
+  // concurrentLookupRequest: 100,
+  // useTls: false,
+  // tlsTrustCertsFilePath: '/path/to/ca-cert.pem',
+  // tlsValidateHostname: false,
+  // tlsAllowInsecureConnection: false,
+  // statsIntervalInSeconds: 60,
+};
 
-export const client = new PulsarClient.Client({ serviceUrl: URL });
+if (pulsar_token) {
+  clientParams["authentication"] = new PulsarClient.AuthenticationToken({
+    token: pulsar_token,
+  });
+}
+
+export const client = new PulsarClient.Client(clientParams);
 
 export const producer = await client
   .createProducer({
     topic: pulsar_topic,
     sendTimeoutMs: pulsar_send_timeout_ms,
     maxPendingMessages: pulsar_max_pending_messages,
-    blockIfQueueFull: pulsar_block_if_queue_full === "true",
+    blockIfQueueFull: pulsar_block_if_queue_full == "1",
   })
   .catch(console.error);
 
@@ -34,6 +51,7 @@ export default class Pulsar {
     }
 
     // Seems like there are some issues with batched sending so we await every message instead
+    // May be not related, worth checking https://github.com/apache/pulsar-client-node/issues/230
     //
     // events.forEach(event => {
     //   this.send(event);
