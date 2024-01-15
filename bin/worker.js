56dc9dcd09af1db27fabf589ade739b80b2614e5
Viktor Vsk <me@viktorvsk.com>
2024-01-15 12:48:45 +0100

    Extract Worker class

--- bin/worker.js
+++ bin/worker.js
@@ -1,13 +1,10 @@
 import http from "node:http";
 import https from "node:https";
 import CacheableLookup from "cacheable-lookup";
-import crypto from "crypto";
-
-import RelayCrawler from "../src/relay_crawler.js";
-import redisClient from "../src/redis.js";
 import { client as pulsarClient, producer } from "../src/pulsar.js";
-import { ts } from "../src/utils.js";
-import { worker_main_loop_interval, worker_max_relays } from "../src/settings.js";
+import redisClient from "../src/redis.js";
+
+import Worker from "../src/worker.js";
 
 const cacheable = new CacheableLookup({
   lookup: false,
@@ -17,72 +14,16 @@ cacheable.servers = (process.env.KNOWSTR_DNS_SERVERS || "1.1.1.1,1.0.0.1,8.8.8.8
 cacheable.install(http.globalAgent);
 cacheable.install(https.globalAgent);
 
-let exiting;
+const worker = new Worker();
 
-const wid = crypto.randomBytes(16).toString("hex");
-
-const [_worker, idle] = await Promise.all([redisClient.SADD("workers", wid), redisClient.get("idle")]);
-
-let relays = [];
-
-let mainLoopInterval = setInterval(main, worker_main_loop_interval);
-
-if (idle) {
-  cleanup("workerInactive");
-}
-
-async function main() {
-  if (exiting) {
-    return;
-  }
-
-  const [_ping, isActive, wrids, restartIds, idle] = await Promise.all([
-    redisClient.HSET("workers_ping", wid, ts().toString()),
-    redisClient.SISMEMBER("workers", wid),
-    redisClient.SMEMBERS(`workers:${wid}`),
-    redisClient.SMEMBERS(`restart_relays_ids`),
-    redisClient.get("idle"),
-  ]);
-
-  if (!isActive || idle) {
-    cleanup(isActive ? "workersIdle" : "workerInactive");
-  }
-
-  await Promise.allSettled(relays.filter((r) => !wrids.includes(r.id) || restartIds.includes(r.id)).map((r) => r.stop()));
-
-  const restartedIds = relays.filter((r) => restartIds.includes(r.id)).map((r) => r.id);
-
-  relays = relays.filter((r) => wrids.includes(r.id) && !restartIds.includes(r.id));
-
-  await Promise.all(restartIds.map((rid) => redisClient.SREM("restart_relays_ids", rid)));
-
-  const currentlyActiveRelaysIds = relays.map((r) => r.id);
-
-  wrids.filter((rid) => !currentlyActiveRelaysIds.includes(rid)).forEach((rid) => relays.push(new RelayCrawler(rid)));
-
-  const relaysToStartCount = worker_max_relays - relays.filter((r) => r.active || r.starting).length;
-
-  relays
-    .filter((r) => r.shouldStart())
-    .sort(() => (Math.random() > 0.5 ? 1 : -1))
-    .slice(0, relaysToStartCount)
-    .forEach((r) => r.start());
-}
+worker.checkIn(cleanup);
 
 async function cleanup(eventType) {
-  if (exiting) {
-    return;
-  }
-  console.log(`Exiting because ${eventType}`);
-  exiting = true;
-  clearInterval(mainLoopInterval);
+  await worker.stop();
+  await Promise.allSettled([producer.flush(), redisClient.quit()]);
 
-  await Promise.allSettled([relays.map((r) => r.stop())].flat());
-
-  await producer?.flush();
-  await producer?.close();
-
-  await Promise.all([redisClient.HDEL("workers_ping", wid), redisClient.DEL(`workers:${wid}`), redisClient.SREM("workers", wid), redisClient.quit(), pulsarClient.close()]);
+  await producer.close();
+  await pulsarClient.close();
 
   process.exit();
 }
---
+++ src/worker.js
@@ -0,0 +1,75 @@
+import crypto from "crypto";
+
+import RelayCrawler from "./relay_crawler.js";
+import redisClient from "./redis.js";
+import { ts } from "./utils.js";
+import { worker_main_loop_interval, worker_max_relays } from "./settings.js";
+
+export default class Worker {
+  constructor() {
+    this.wid = crypto.randomBytes(16).toString("hex");
+    this.exiting = false;
+    this.relays = [];
+  }
+
+  async checkIn(cleanup) {
+    const [_worker, idle] = await Promise.all([redisClient.SADD("workers", this.wid), redisClient.get("idle")]);
+
+    if (idle) {
+      cleanup("workersIdle");
+    }
+
+    this.cleanup = cleanup;
+
+    this.mainLoopInterval = setInterval(this.run.bind(this), worker_main_loop_interval);
+  }
+
+  async run() {
+    if (this.exiting) {
+      return;
+    }
+
+    const [_ping, isActive, wrids, restartIds, idle] = await Promise.all([
+      redisClient.HSET("workers_ping", this.wid, ts().toString()),
+      redisClient.SISMEMBER("workers", this.wid),
+      redisClient.SMEMBERS(`workers:${this.wid}`),
+      redisClient.SMEMBERS(`restart_relays_ids`),
+      redisClient.get("idle"),
+    ]);
+
+    if (!isActive || idle) {
+      this.cleanup(isActive ? "workersIdle" : "workerInactive");
+    }
+
+    await Promise.allSettled(this.relays.filter((r) => !wrids.includes(r.id) || restartIds.includes(r.id)).map((r) => r.stop()));
+
+    const restartedIds = this.relays.filter((r) => restartIds.includes(r.id)).map((r) => r.id);
+
+    this.relays = this.relays.filter((r) => wrids.includes(r.id) && !restartIds.includes(r.id));
+
+    await Promise.all(restartIds.map((rid) => redisClient.SREM("restart_relays_ids", rid)));
+
+    const currentlyActiveRelaysIds = this.relays.map((r) => r.id);
+
+    wrids.filter((rid) => !currentlyActiveRelaysIds.includes(rid)).forEach((rid) => this.relays.push(new RelayCrawler(rid)));
+
+    const relaysToStartCount = worker_max_relays - this.relays.filter((r) => r.active || r.starting).length;
+
+    await this.relays
+      .filter((r) => r.shouldStart())
+      .sort(() => (Math.random() > 0.5 ? 1 : -1))
+      .slice(0, relaysToStartCount)
+      .map((r) => r.start());
+  }
+
+  async stop() {
+    if (this.exiting) {
+      return;
+    }
+
+    this.exiting = true;
+    clearInterval(this.mainLoopInterval);
+    await Promise.allSettled(this.relays.map((r) => r.stop()));
+    await Promise.all([redisClient.HDEL("workers_ping", this.wid), redisClient.DEL(`workers:${this.wid}`), redisClient.SREM("workers", this.wid)]);
+  }
+}
